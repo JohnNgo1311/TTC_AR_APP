3f98e978a813230f67b6c7ab5bbd37f35ded44ee
Ngo Xuan Nghia <JohnNgo1311@gmail.com>
2025-02-21 16:40:10 +0700

    push code 16:39 21/2/2025 Fix Update Device - Test - 90%

--- TTC_AR/Assets/Scripts/Add_Item_For_Update.cs
+++ TTC_AR/Assets/Scripts/Add_Item_For_Update.cs
@@ -1,85 +1,328 @@
+using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Reflection;
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class Add_Item_For_Update : MonoBehaviour
 {
-    public GameObject ParentVerticalObject;
-    public GameObject SelectionList;
-    public GameObject Horizontal_Item_Selection_Prefab;
-    public GameObject Horizontal_Item_Selection_Available;
+    [Header("General")]
+    public GameObject parent_Content_Vertical_Group;
+    public ScrollRect scrollRect;
+    public RectTransform viewPortTransform;
+    public List<LayoutElement> layoutElements = new List<LayoutElement>();
 
-    private List<GameObject> List_Selected_GameObject = new List<GameObject>();
-    [SerializeField]
-    private int Item_Selected_Count = 0;  //! Tổng số Item đã được chọn, chỉ tăng sau khi Item được gán giá trị
-    [SerializeField]
-    private int List_Horizontal_Item_Count; //! Tổng số horinzontal Group đã được tạo
-    private Sprite Blue_Add_Remove_Item_Background_Temp;
-    //  private Sprite Light_Blue_Add_Remove_Item_Background_Temp;
-    private int index;
+
+    [Header("List Selection Panel")]
+    public GameObject selection_List_Device_Panel;
+    public GameObject selection_List_ModuleIO_Panel;
+    public GameObject selection_List_Location_Image_Panel;
+    public GameObject selection_List_Connection_Image_Panel;
+
+    public Transform Device_List_Selection_Option_Transform;
+    public Transform ModuleIO_List_Selection_Option_Transform;
+    public Transform Location_Image_List_Selection_Option_Transform;
+    public Transform Connection_Image_List_Selection_Option_Transform;
+
+    private GameObject Device_Selection_Option_Initial;
+    private GameObject ModuleIO_Selection_Option_Initial;
+    private GameObject Location_Image_Selection_Option_Initial;
+    private GameObject Connection_Image_Selection_Option_Initial;
+
+
+    [Header("Device Selection Item")]
+    public GameObject Device_Parent_GridLayout_Group;
+    public GameObject Device_Item_Prefab;
+    private List<GameObject> Device_selected_GameObjects = new List<GameObject>();
+    private int Device_Selected_Count = 0;
+
+    [Header("ModuleIO Selection Item")]
+    public GameObject ModuleIO_Parent_GridLayout_Group;
+    public GameObject ModuleIO_Item_Prefab;
+    private List<GameObject> ModuleIO_selected_GameObjects = new List<GameObject>();
+    private int ModuleIO_Selected_Count = 0;
+
+
+    [Header("Location_Image Selection Item")]
+    public GameObject Location_Image_Parent_VerticalLayout_Group;
+    public GameObject Location_Image_Item_Prefab;
+    private List<GameObject> Location_Image_selected_GameObjects = new List<GameObject>();
+    private int Location_Image_Selected_Count = 0;
+
+    [Header("Connection_Image Selection Item")]
+    public GameObject Connection_Image_Parent_VerticalLayout_Group;
+    public GameObject Connection_Image_Item_Prefab;
+    private List<GameObject> Connection_Image_selected_GameObjects = new List<GameObject>();
+    private int Connection_Image_Selected_Count = 0;
+
+    private Transform temp_Item_Transform;
+    // private Dictionary<int, string> item_Text_Value_Dictionary = new Dictionary<int, string>();
 
     private void Start()
     {
-        Blue_Add_Remove_Item_Background_Temp = Resources.Load<Sprite>("images/UIimages/Blue_item_add_or_remove_Background");
-        if (Blue_Add_Remove_Item_Background_Temp == null)
-        {
-            Debug.LogError("Blue_Add_Remove_Item_Background_Temp is null");
-        }
-        else
-        {
-            Debug.Log("Blue_Add_Remove_Item_Background_Temp is not null");
-        }
-        // Light_Blue_Add_Remove_Item_Background_Temp = Resources.Load<Sprite>("images/UIimages/light_Blue_Add_Remove_Item_Background.png");
-        List_Horizontal_Item_Count = ParentVerticalObject.transform.childCount - 1;
-        Debug.Log("List_Horizontal_Item_Count: " + List_Horizontal_Item_Count);
+        InitializeItemOption();
+        Populate_List_Selection_JB_TSD();
+        scrollRect.normalizedPosition = new Vector2(0, 1);
+        AdjustLayoutElements();
     }
 
-    public void OpenListSelection()
+    private void InitializeItemOption()
     {
-        Debug.Log("Item_Selected_Count " + Item_Selected_Count);
-        if (Item_Selected_Count == 0)
-        {
-            SetItemBackground(Horizontal_Item_Selection_Available.transform.GetChild(0), Blue_Add_Remove_Item_Background_Temp);
-        }
-        else if (Item_Selected_Count < 4)
-        {
-            SetItemBackground(Horizontal_Item_Selection_Available.transform.GetChild(Item_Selected_Count), Blue_Add_Remove_Item_Background_Temp);
-        }
-        else if (Item_Selected_Count % 4 == 0)
-        {
-            GameObject new_Horizontal_Item_Selection = Instantiate(Horizontal_Item_Selection_Prefab, ParentVerticalObject.transform);
-            SetItemBackground(new_Horizontal_Item_Selection.transform.GetChild(0), Blue_Add_Remove_Item_Background_Temp);
-            List_Horizontal_Item_Count++;
-            Debug.Log("List_Horizontal_Item_Count: " + List_Horizontal_Item_Count);
-        }
-        else
-        {
-            SetItemBackground(ParentVerticalObject.transform.GetChild(List_Horizontal_Item_Count).GetChild(Item_Selected_Count % 4), Blue_Add_Remove_Item_Background_Temp);
-        }
-        SelectionList.SetActive(true);
+        Device_Selection_Option_Initial = Device_List_Selection_Option_Transform.GetChild(0).gameObject;
+        ModuleIO_Selection_Option_Initial = ModuleIO_List_Selection_Option_Transform.GetChild(0).gameObject;
+        Location_Image_Selection_Option_Initial = Location_Image_List_Selection_Option_Transform.GetChild(0).gameObject;
+        Connection_Image_Selection_Option_Initial = Connection_Image_List_Selection_Option_Transform.GetChild(0).gameObject;
+
+    }
+    private void AdjustLayoutElements()
+    {
+        float viewPortHeight = viewPortTransform.rect.height;
+        layoutElements[0].minHeight = viewPortHeight * 0.12f;
+        layoutElements[1].minHeight = viewPortHeight * 0.12f;
+        layoutElements[2].minHeight = viewPortHeight * 0.6f;
     }
 
-    private void SetItemBackground(Transform itemTransform, Sprite background)
+    private void Populate_List_Selection_JB_TSD()
     {
-        itemTransform.GetComponent<Image>().sprite = background;
+        if (!selection_List_Device_Panel.activeSelf) selection_List_Device_Panel.SetActive(true);
+        if (!selection_List_ModuleIO_Panel.activeSelf) selection_List_ModuleIO_Panel.SetActive(true);
+        if (!selection_List_Location_Image_Panel.activeSelf) selection_List_Location_Image_Panel.SetActive(true);
+        if (!selection_List_Connection_Image_Panel.activeSelf) selection_List_Connection_Image_Panel.SetActive(true);
+
+
+        List<string> List_DeviceName = GlobalVariable.list_DeviceName;
+        List<string> List_ModuleIOName = GlobalVariable.list_ModuleIOName;
+
+        List<string> List_LocationImageName = GlobalVariable.list_ImageName;
+        List<string> List_ConnectionImageName = GlobalVariable.list_ImageName;
+
+        if (List_DeviceName != null && List_DeviceName.Count > 0)
+        {
+            // item_Text_Value_Dictionary.Clear();
+            if (List_DeviceName.Count == 1)
+            {
+                AddItemOption(Device_Selection_Option_Initial, List_DeviceName[0], "Device");
+            }
+            else
+            {
+                foreach (var DeviceName in List_DeviceName)
+                {
+                    GameObject new_Device_Option = Instantiate(Device_Selection_Option_Initial, Device_List_Selection_Option_Transform);
+                    AddItemOption(new_Device_Option, DeviceName, "Device");
+                }
+                Device_Selection_Option_Initial.SetActive(false);
+            }
+        }
+        selection_List_Device_Panel.SetActive(false);
+
+        if (List_ModuleIOName != null && List_ModuleIOName.Count > 0)
+        {
+            // item_Text_Value_Dictionary.Clear();
+            if (List_ModuleIOName.Count == 1)
+            {
+                AddItemOption(ModuleIO_Selection_Option_Initial, List_ModuleIOName[0], "ModuleIO");
+            }
+            else
+            {
+                foreach (var ModuleIOName in List_ModuleIOName)
+                {
+                    GameObject new_ModuleIO_Option = Instantiate(ModuleIO_Selection_Option_Initial, ModuleIO_List_Selection_Option_Transform);
+                    AddItemOption(new_ModuleIO_Option, ModuleIOName, "ModuleIO");
+                }
+                ModuleIO_Selection_Option_Initial.SetActive(false);
+            }
+        }
+        selection_List_ModuleIO_Panel.SetActive(false);
+
+        if (List_LocationImageName != null && List_LocationImageName.Count > 0)
+        {
+            // item_Text_Value_Dictionary.Clear();
+            if (List_LocationImageName.Count == 1)
+            {
+                AddItemOption(Location_Image_Selection_Option_Initial, List_LocationImageName[0], "Location_Image");
+            }
+            else
+            {
+                foreach (var ImageName in List_LocationImageName)
+                {
+                    GameObject new_Image_Option = Instantiate(Location_Image_Selection_Option_Initial, Location_Image_List_Selection_Option_Transform);
+                    AddItemOption(new_Image_Option, ImageName, "Location_Image");
+                }
+                Location_Image_Selection_Option_Initial.SetActive(false);
+            }
+        }
+        selection_List_Location_Image_Panel.SetActive(false);
+
+        if (List_ConnectionImageName != null && List_ConnectionImageName.Count > 0)
+        {
+            // item_Text_Value_Dictionary.Clear();
+            if (List_ConnectionImageName.Count == 1)
+            {
+                AddItemOption(Connection_Image_Selection_Option_Initial, List_ConnectionImageName[0], "Connection_Image");
+            }
+            else
+            {
+                foreach (var ImageName in List_ConnectionImageName)
+                {
+                    GameObject new_Image_Option = Instantiate(Connection_Image_Selection_Option_Initial, Connection_Image_List_Selection_Option_Transform);
+                    AddItemOption(new_Image_Option, ImageName, "Connection_Image");
+                }
+                Connection_Image_Selection_Option_Initial.SetActive(false);
+            }
+        }
+        selection_List_Connection_Image_Panel.SetActive(false);
     }
 
-    private void SelectItem()
+    private void AddItemOption(GameObject item, string text, string field)
     {
-        Item_Selected_Count++;
-        Debug.Log("Select Item" + Item_Selected_Count);
+        SetItemOptionText(item, text);
+        AddButtonListener(item, () => SelectItem(text, field));
     }
 
-    private void DeselectItem()
+    private void SetItemOptionText(GameObject item, string text)
     {
-        Item_Selected_Count--;
-        Debug.Log("Deselect Item" + Item_Selected_Count);
+        TMP_Text textComponent = item.GetComponentInChildren<TMP_Text>();
+        if (textComponent != null)
+        {
+            textComponent.text = text;
+        }
+    }
+
+    private void AddButtonListener(GameObject item, UnityEngine.Events.UnityAction action)
+    {
+        Button button = item.GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(action);
+        }
+    }
+
+    public void OpenListDeviceSelection()
+    {
+        GameObject newDeviceItem = Instantiate(Device_Item_Prefab, Device_Parent_GridLayout_Group.transform);
+        temp_Item_Transform = newDeviceItem.transform;
+        temp_Item_Transform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => DeselectItem(newDeviceItem, "Device"));
+        selection_List_Device_Panel.SetActive(true);
+        Device_Parent_GridLayout_Group.transform.GetChild(0).gameObject.SetActive(false);
+    }
+    public void OpenListModuleSelection()
+    {
+        GameObject newModuleItem = Instantiate(ModuleIO_Item_Prefab, ModuleIO_Parent_GridLayout_Group.transform);
+        temp_Item_Transform = newModuleItem.transform;
+        temp_Item_Transform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => DeselectItem(newModuleItem, "ModuleIO"));
+        selection_List_ModuleIO_Panel.SetActive(true);
+        ModuleIO_Parent_GridLayout_Group.transform.GetChild(0).gameObject.SetActive(false);
+    }
+    public void OpenListImageLocationSelection()
+    {
+        GameObject new_Location_Image_Item = Instantiate(Location_Image_Item_Prefab, Location_Image_Parent_VerticalLayout_Group.transform);
+        temp_Item_Transform = new_Location_Image_Item.transform;
+        temp_Item_Transform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => DeselectItem(new_Location_Image_Item, "Location_Image"));
+        selection_List_Location_Image_Panel.SetActive(true);
+    }
+    public void OpenListImageConnectionSelection()
+    {
+        GameObject new_Connection_Image_Item = Instantiate(Connection_Image_Item_Prefab, Connection_Image_Parent_VerticalLayout_Group.transform);
+        temp_Item_Transform = new_Connection_Image_Item.transform;
+        temp_Item_Transform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => DeselectItem(new_Connection_Image_Item, "Connection_Image"));
+        selection_List_Connection_Image_Panel.SetActive(true);
+    }
+
+    private void SetItemTextValue(Transform temp_Item_Transform, string textValue)
+    {
+        TMP_Text textComponent = temp_Item_Transform.GetComponentInChildren<TMP_Text>();
+        if (textComponent != null)
+        {
+            textComponent.text = textValue;
+        }
+        Debug.Log("Text Value: " + textValue);
+    }
+
+    private void SelectItem(string textValue, string Field)
+    {
+        SetItemTextValue(temp_Item_Transform, textValue);
+        // Blue_Item_Selected_Count++;
+        // Debug.Log("Select Item" + Blue_Item_Selected_Count);
+        CloseListSelection();
+        switch (Field)
+        {
+            case "Device":
+                Device_Selected_Count++;
+                if (!Device_selected_GameObjects.Contains(temp_Item_Transform.gameObject))
+                {
+                    Device_selected_GameObjects.Add(temp_Item_Transform.gameObject);
+                }
+                break;
+            case "ModuleIO":
+                ModuleIO_Selected_Count++;
+                if (!ModuleIO_selected_GameObjects.Contains(temp_Item_Transform.gameObject))
+                {
+                    ModuleIO_selected_GameObjects.Add(temp_Item_Transform.gameObject);
+                }
+                break;
+            case "Location_Image":
+                Location_Image_Selected_Count++;
+                if (!Location_Image_selected_GameObjects
+                    .Contains(temp_Item_Transform.gameObject))
+                {
+                    Location_Image_selected_GameObjects.Add(temp_Item_Transform.gameObject);
+                }
+                break;
+            case "Connection_Image":
+                Connection_Image_Selected_Count++;
+                if (!Connection_Image_selected_GameObjects
+                    .Contains(temp_Item_Transform.gameObject))
+                {
+                    Connection_Image_selected_GameObjects.Add(temp_Item_Transform.gameObject);
+                }
+                break;
+        }
+
+    }
+
+    private void DeselectItem(GameObject item, string field)
+    {
+        // if (Device_selected_GameObjects.Contains(item))
+        // {
+        //     Device_selected_GameObjects.Remove(item);
+        //     Destroy(item);
+        //     Debug.Log("Deselect Item - Count: " + Blue_Item_Selected_Count);
+        //     CloseListSelection();
+        // }
+        switch (field)
+        {
+            case "Device":
+                Device_Selected_Count--;
+                Device_selected_GameObjects.Remove(item);
+                Destroy(item);
+                break;
+            case "ModuleIO":
+                ModuleIO_Selected_Count--;
+                ModuleIO_selected_GameObjects.Remove(item);
+                Destroy(item);
+                break;
+            case "Location_Image":
+                Location_Image_Selected_Count--;
+                Location_Image_selected_GameObjects.Remove(item);
+                Destroy(item);
+                break;
+            case "Connection_Image":
+                Connection_Image_Selected_Count--;
+                Connection_Image_selected_GameObjects.Remove(item);
+                Destroy(item);
+                break;
+        }
+        CloseListSelection();
     }
 
     public void CloseListSelection()
     {
-        SelectItem();
-        SelectionList.SetActive(false);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(parent_Content_Vertical_Group.GetComponent<RectTransform>());
+        Canvas.ForceUpdateCanvases();
+        if (selection_List_Device_Panel.activeSelf) selection_List_Device_Panel.SetActive(false);
+        if (selection_List_ModuleIO_Panel.activeSelf) selection_List_ModuleIO_Panel.SetActive(false);
+        if (selection_List_Location_Image_Panel.activeSelf) selection_List_Location_Image_Panel.SetActive(false);
+        if (selection_List_Connection_Image_Panel.activeSelf) selection_List_Connection_Image_Panel.SetActive(false);
     }
 }
---
+++ TTC_AR/Assets/Scripts/Add_Item_For_Update_Optional.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Add_Item_For_Update_Optional : MonoBehaviour
+{
+    public GameObject parentVerticalObject;
+    public GameObject selectionList;
+    public GameObject horizontal_Item_Selection_Prefab;
+    public GameObject horizontal_Item_Selection_Available;
+    public Transform Content_List_Selection_Transform;
+    private GameObject Item_Option_Initial;
+    private List<GameObject> selectedGameObjects = new List<GameObject>();
+    [SerializeField]
+    private int itemSelectedCount = 0;
+    [SerializeField]
+    private int horizontalItemCount;
+    private Sprite blue_Add_Remove_Item_Background;
+    private Transform itemTransform;
+    private string item_Text_Value;
+    private Dictionary<int, string> item_Text_Value_Dictionary = new Dictionary<int, string>();
+
+    private void Start()
+    {
+        LoadResources();
+        InitializeItemOption();
+        InitializeHorizontalItemCount();
+        PopulateItemList();
+    }
+
+    private void LoadResources()
+    {
+        blue_Add_Remove_Item_Background = Resources.Load<Sprite>("images/UIimages/Blue_item_add_or_remove_Background");
+        Debug.Log(blue_Add_Remove_Item_Background == null ? "Blue_Add_Remove_Item_Background_Temp is null" : "Blue_Add_Remove_Item_Background_Temp is not null");
+    }
+
+    private void InitializeItemOption()
+    {
+        Item_Option_Initial = Content_List_Selection_Transform.GetChild(0).gameObject;
+    }
+
+    private void InitializeHorizontalItemCount()
+    {
+        horizontalItemCount = parentVerticalObject.transform.childCount - 1; // Exclude HorizontalItemSelectionAvailable
+        Debug.Log("Horizontal_Item_Count: " + horizontalItemCount);
+    }
+
+    private void PopulateItemList()
+    {
+        selectionList.SetActive(true);
+        List<string> list_JBName = GlobalVariable.list_jBName;
+        if (list_JBName != null && list_JBName.Count > 0)
+        {
+            item_Text_Value_Dictionary.Clear();
+
+            if (list_JBName.Count == 1)
+            {
+                item_Text_Value_Dictionary.Add(0, list_JBName[0]);
+                SetItemOptionText(Item_Option_Initial, list_JBName[0]);
+                AddButtonListener(Item_Option_Initial, () => SelectItem(list_JBName[0]));
+            }
+            else
+            {
+                foreach (var JB_Name in list_JBName)
+                {
+                    GameObject new_Item = Instantiate(Item_Option_Initial, Content_List_Selection_Transform);
+                    SetItemOptionText(new_Item, JB_Name);
+                    AddButtonListener(new_Item, () => SelectItem(JB_Name));
+                }
+                Item_Option_Initial.SetActive(false);
+            }
+        }
+        selectionList.SetActive(false);
+    }
+
+    private void SetItemOptionText(GameObject item, string text)
+    {
+        TMP_Text textComponent = item.GetComponentInChildren<TMP_Text>();
+        if (textComponent != null)
+        {
+            textComponent.text = text;
+        }
+    }
+
+    private void AddButtonListener(GameObject item, UnityEngine.Events.UnityAction action)
+    {
+        Button button = item.GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(action);
+        }
+    }
+
+    public void OpenListSelection()
+    {
+        Debug.Log("Item_Selected_Count " + itemSelectedCount);
+        if (itemSelectedCount == 0)
+        {
+            itemTransform = horizontal_Item_Selection_Available.transform.GetChild(0);
+        }
+        else if (itemSelectedCount < 4)
+        {
+            itemTransform = horizontal_Item_Selection_Available.transform.GetChild(itemSelectedCount);
+        }
+        else if (itemSelectedCount % 4 == 0)
+        {
+            GameObject newHorizontalItemSelection = Instantiate(horizontal_Item_Selection_Prefab, parentVerticalObject.transform);
+            itemTransform = newHorizontalItemSelection.transform.GetChild(0);
+            horizontalItemCount++;
+            Debug.Log("Horizontal_Item_Count: " + horizontalItemCount);
+        }
+        else
+        {
+            itemTransform = parentVerticalObject.transform.GetChild(horizontalItemCount).GetChild(itemSelectedCount % 4);
+        }
+        SetItemBackground(itemTransform, blue_Add_Remove_Item_Background);
+        var tempItemTransform = itemTransform;
+        itemTransform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => DeselectItem(tempItemTransform.gameObject));
+        selectionList.SetActive(true);
+    }
+
+    private void SetItemBackground(Transform itemTransform, Sprite background)
+    {
+        Image imageComponent = itemTransform.GetComponent<Image>();
+        if (imageComponent != null)
+        {
+            imageComponent.sprite = background;
+        }
+    }
+
+    private void SetItemTextValue(Transform itemTransform, string textValue)
+    {
+        TMP_Text textComponent = itemTransform.GetComponentInChildren<TMP_Text>();
+        if (textComponent != null)
+        {
+            textComponent.text = textValue;
+        }
+        Debug.Log("Text Value: " + textValue);
+    }
+
+    private void SelectItem(string textValue)
+    {
+        SetItemTextValue(itemTransform, textValue);
+        itemSelectedCount++;
+        Debug.Log("Select Item" + itemSelectedCount);
+        CloseListSelection();
+        if (!selectedGameObjects.Contains(itemTransform.gameObject)) selectedGameObjects.Add(itemTransform.gameObject);
+
+    }
+
+    private void DeselectItem(GameObject item)
+    {
+        if (selectedGameObjects.Contains(item))
+        {
+            selectedGameObjects.Remove(item);
+            itemSelectedCount = Mathf.Max(0, itemSelectedCount - 1);
+
+            TMP_Text textComponent = item.GetComponentInChildren<TMP_Text>();
+            if (textComponent != null)
+            {
+                textComponent.text = "";
+            }
+
+            item.SetActive(false);
+            Debug.Log("Deselect Item - Count: " + itemSelectedCount);
+        }
+    }
+
+
+
+    public void CloseListSelection()
+    {
+        Resize_Gameobject_Function.Resize_Parent_GameObject(parentVerticalObject.GetComponent<RectTransform>());
+        selectionList.SetActive(false);
+    }
+}
